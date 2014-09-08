ac05013c14b16aeafc447272f3de7702bc4d8c1e
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2014-09-07 20:48:08 -0400

    'Desc' operator method rewritten to an iterative algorithm.

--- test/spec/descCursorSpec.js
+++ test/spec/descCursorSpec.js
@@ -45,4 +45,116 @@ describe("DescCursor", function () {
     expect(descelement[5]).toEqual(6);
     expect(descelement[6]).toEqual(2);
   });
+
+  it("desc limit", function () {
+
+
+    var tree = new Tree(10, function(deletedelement) {
+
+    });
+
+    /*
+     *              50
+     *             /  \
+     *            /    \
+     *           6     76
+     *          /     /  \
+     *         2     55  98
+     *                    \
+     *                    100
+     */
+    tree.insert(50, "test");
+    tree.insert(6, "test");
+    tree.insert(98, "test");
+    tree.insert(2, "test2");
+    tree.insert(76, "test");
+    tree.insert(55, "test");
+    tree.insert(100, "test");
+
+    debugger;
+    var descelement = tree.desc(4);
+
+    expect(descelement).toBeDefined();
+
+    expect(descelement[0]).toEqual(100);
+    expect(descelement[1]).toEqual(98);
+    expect(descelement[2]).toEqual(76);
+    expect(descelement[3]).toEqual(55);
+    
+    expect(descelement.length).toEqual(4);
+  });
+
+  it("desc limit #2", function () {
+
+
+    var tree = new Tree(10, function(deletedelement) {
+
+    });
+
+    /*
+     *              50
+     *             /  \
+     *            /    \
+     *           6     76
+     *          /     /  \
+     *         2     55  98
+     *                    \
+     *                    100
+     */
+    tree.insert(50, "test");
+    tree.insert(6, "test");
+    tree.insert(98, "test");
+    tree.insert(2, "test2");
+    tree.insert(76, "test");
+    tree.insert(55, "test");
+    tree.insert(100, "test");
+
+    var descelement = tree.desc(5);
+
+    expect(descelement).toBeDefined();
+
+    expect(descelement[0]).toEqual(100);
+    expect(descelement[1]).toEqual(98);
+    expect(descelement[2]).toEqual(76);
+    expect(descelement[3]).toEqual(55);
+    expect(descelement[4]).toEqual(50);
+    
+    expect(descelement.length).toEqual(5);
+  });
+
+  it("desc limit #3", function () {
+
+
+    var tree = new Tree(10, function(deletedelement) {
+
+    });
+
+    /*
+     *              50
+     *             /  \
+     *            /    \
+     *           6     76
+     *          /     /  \
+     *         2     55  98
+     *                    \
+     *                    100
+     */
+    tree.insert(50, "test");
+    tree.insert(6, "test");
+    tree.insert(98, "test");
+    tree.insert(2, "test2");
+    tree.insert(76, "test");
+    tree.insert(55, "test");
+    tree.insert(100, "test");
+
+    var descelement = tree.desc(2);
+
+    expect(descelement).toBeDefined();
+
+    expect(descelement[0]).toEqual(100);
+    expect(descelement[1]).toEqual(98);
+    
+    expect(descelement.length).toEqual(2);
+  });
+
 });
